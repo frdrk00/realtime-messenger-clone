a13ddc40e122c788aa0b9442545a133072bb76c4
frdrk00 <masterkooo@gmail.com>
2023-05-08 18:55:45 +0200

    Real-time messages, conversations, read receipts and active status with Pusher

---
+++ app/components/ActiveStatus.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import { FC } from "react";
+import useActiveChannel from "../hooks/useActiveChannel";
+
+interface ActiveStatusProps {}
+
+const ActiveStatus: FC<ActiveStatusProps> = ({}) => {
+    useActiveChannel()
+
+  return null
+};
+
+export default ActiveStatus;
